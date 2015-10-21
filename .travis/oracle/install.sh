df3d063ebd0ac042877ce6721bec4f760be5f707
Rapševičius Valdas <vrapsevicius@gmail.com>
2015-10-21 11:37:34 +0300

    Modified Oracle footprint settings

--- .travis/oracle/install.sh
+++ .travis/oracle/install.sh
@@ -23,9 +23,9 @@ sudo usermod -aG dba $USER
 
 ( echo ; echo ; echo travis ; echo travis ; echo n ) | sudo AWK='/usr/bin/awk' /etc/init.d/oracle-xe configure
 
-"$ORACLE_HOME/bin/sqlplus" -L -S / AS SYSDBA <<SQL
-alter system set memory_max_target = 140m;
-alter system set memory_target = 140m;
+"$ORACLE_HOME/bin/sqlplus" -L / AS SYSDBA <<SQL
+alter system set memory_max_target = 172m scope = spfile;
+alter system set memory_target = 172m;
 ALTER SYSTEM SET SGA_TARGET = 0;
 ALTER SYSTEM SET PGA_AGGREGATE_TARGET = 0;
 SQL
