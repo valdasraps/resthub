a645d8bbd0ba39ddab164ec2f395f6612b803dd7
Rapševičius Valdas <vrapsevicius@gmail.com>
2016-04-18 13:57:40 +0300

    Switched travis to secure oracle download

--- .travis/oracle/download.sh
+++ .travis/oracle/download.sh
@@ -2,7 +2,9 @@
 
 [ -n "$ORACLE_FILE" ] || { echo "Missing ORACLE_FILE environment variable!"; exit 1; }
 [ -n "$ORACLE_URL" ] || { echo "Missing ORACLE_URL environment variable!"; exit 1; }
+[ -n "$ORACLE_URL_USER" ] || { echo "Missing ORACLE_URL_USER environment variable!"; exit 1; }
+[ -n "$ORACLE_URL_PASS" ] || { echo "Missing ORACLE_URL_PASS environment variable!"; exit 1; }
 
 cd "$(dirname "$(readlink -f "$0")")"
 
-wget "$ORACLE_URL"
+wget --user="$ORACLE_URL_USER" --password="$ORACLE_URL_PASS" "$ORACLE_URL"
