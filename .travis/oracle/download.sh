fad37f30eb077928cb20ee4dec0bc02542eb6440
Rapševičius Valdas <vrapsevicius@gmail.com>
2016-04-18 13:36:29 +0300

    Simplified travis script

--- .travis/oracle/download.sh
+++ .travis/oracle/download.sh
@@ -1,16 +1,8 @@
 #!/bin/sh -e
 
-[ -n "$ORACLE_COOKIE" ] || { echo "Missing ORACLE_COOKIE environment variable!"; exit 1; }
 [ -n "$ORACLE_FILE" ] || { echo "Missing ORACLE_FILE environment variable!"; exit 1; }
+[ -n "$ORACLE_URL" ] || { echo "Missing ORACLE_URL environment variable!"; exit 1; }
 
 cd "$(dirname "$(readlink -f "$0")")"
 
-npm install bluebird phantomjs node-phantom-simple
-
-export COOKIES='cookies.txt'
-export USER_AGENT='Mozilla/5.0'
-
-echo > "$COOKIES"
-chmod 600 "$COOKIES"
-
-exec node download.js
+wget "$ORACLE_URL"
